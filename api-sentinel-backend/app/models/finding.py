6f470846a75c83cbac773787f2986afd50d2b6cb
Harshittomar <tomarharshit2023@gmail.com>
2026-06-26 23:31:25 +0530

    Added new sqlAlchamey's relationships

--- api-sentinel-backend/app/models/finding.py
+++ api-sentinel-backend/app/models/finding.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 from sqlalchemy import DateTime, ForeignKey, String, Text, func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
+from app.models.scan import Scan
 
 class Finding(Base):
     __tablename__ = "findings"
@@ -45,4 +46,8 @@ class Finding(Base):
     created_at : Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
+    )
+
+    scan : Mapped["Scan"] = relationship(
+        back_populates="findings"
     )
--- api-sentinel-backend/app/models/scan.py
+++ api-sentinel-backend/app/models/scan.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 from sqlalchemy import DateTime, Integer, String, func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
+from app.models.finding import Finding
 
 class Scan(Base):
     __tablename__ = "scans"
@@ -56,3 +57,11 @@ class Scan(Base):
         DateTime(timezone=True),
         server_default=func.now(),
     )
+
+    findings: Mapped[list["Finding"]]= relationship(
+        back_populates= "scan",
+        cascade="all , delete-orphan"
+    )
+
+# backpopulates means that suppose we scan has a relationship with findings and findings is also in a relationship with scan so that will tell the orm that these two relationships are same     
+# cascade means that if we delete a scan then should the findings of the particlulare scan be exist, no? so using cascade we can delete that too
