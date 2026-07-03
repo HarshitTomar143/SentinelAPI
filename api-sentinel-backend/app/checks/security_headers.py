ec9a8b3edf4edc4d54badea28084936e529e3da0
Harshittomar <tomarharshit2023@gmail.com>
2026-07-03 13:14:08 +0530

    implemented SecurityHeadersCheck class

--- api-sentinel-backend/app/checks/security_headers.py
+++ api-sentinel-backend/app/checks/security_headers.py
@@ -8,3 +8,34 @@ class SecurityHeadersCheck:
             headers: httpx.Headers,
     )-> list[FindingResult]:
         
+        required_headers = {
+                "Strict-Transport-Security": {
+                        "severity": FindingSeverity.HIGH.value,
+                        "recommendation": "Enable HSTS to enforce HTTPS connections."
+                },
+                "X-Content-Type-Options": {
+                        "severity": FindingSeverity.MEDIUM.value,
+                        "recommendation": "Set X-Content-Type-Options to prevent MIME type sniffing."
+                },
+                "X-Frame-Options": {
+                        "severity": FindingSeverity.LOW.value,
+                        "recommendation": "Set X-Frame-Options to protect against clickjacking."
+                },
+                "Content-Security-Policy": {
+                        "severity": FindingSeverity.LOW.value,
+                        "recommendation": "Define a Content Security Policy to reduce XSS risks."
+                },
+                "Referrer-Policy": {
+                        "severity": FindingSeverity.LOW.value,
+                        "recommendation": "Configure a Referrer-Policy to limit information leakage."
+                },
+                }
+        finding : list[FindingResult] = []
+
+        for header, config in required_headers.items():
+            if header in headers:
+                pass
+            else:
+                pass
+            
+        return finding    
