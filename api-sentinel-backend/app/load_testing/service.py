703e20bb010dff538b6b37962423d2b55e704d8f
Harshittomar <tomarharshit2023@gmail.com>
2026-07-08 12:00:02 +0530

    Completed the service function

--- api-sentinel-backend/app/load_testing/service.py
+++ api-sentinel-backend/app/load_testing/service.py
@@ -1,5 +1,7 @@
 import httpx
-
+from app.load_testing.schemas import HttpRequest
+from app.models.request_result import RequestResult
+import time
 
 class LoadTestService:
 
@@ -13,10 +15,23 @@ class LoadTestService:
             self,
             request: HttpRequest,
     ) -> RequestResult:
-           self.client.request(
+           
+            start = time.perf_counter()
+
+            response = self.client.request(
                 method=request.method.value,
                 url= str(request.url),
                 headers= request.headers,
                 params=request.quesry_params,
                 json=request.body,
            )
+            
+            response_time_ms = (
+            time.perf_counter() - start
+        ) * 1000
+            
+            return RequestResult(
+            status_code=response.status_code,
+            response_time_ms=response_time_ms,
+            timed_out=False,
+        )
